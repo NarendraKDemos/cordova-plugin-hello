9339a74bf81648e43bd5187f852f1f0e3995160f
NarendraKDemos <46570313+NarendraKDemos@users.noreply.github.com>
2019-03-07 11:26:11 +0530

    Update Hello.java

--- src/android/Hello.java
+++ src/android/Hello.java
@@ -13,8 +13,27 @@ public class Hello extends CordovaPlugin {
 
             String name = data.getString(0);
             String message = "Hello, " + name;
-            callbackContext.success(message);
+            //callbackContext.success(message);
+            
+            new AlertDialog.Builder(this.cordova.getActivity())
+                .setTitle("Plugin Demo")
+                .setMessage("My name is " + name)
 
+                // Specifying a listener allows you to take an action before dismissing the dialog.
+                // The dialog is automatically dismissed when a dialog button is clicked.
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Continue with delete operation
+                    }
+                })
+
+                // A null listener allows the button to dismiss the dialog and take no further action.
+                .setNegativeButton(android.R.string.no, null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+            
+            
+            
             return true;
 
         } else {
